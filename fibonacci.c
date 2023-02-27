15c7e995714e91c6cfdda33bdd81a7760d9a0459
Avg38 <mailamandineg@gmail.com>
2023-02-27 23:11:28 +0100

    first commit

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int nb1, nb2, nb3, nb;
+
+    printf("Choisisez un nombre: ");
+    scanf("%d", &nb1);
+
+    printf("Choisisez un nombre de fin: ");
+    scanf("%d", &nb3);
+    nb2 = nb1;
+    printf("%d, %d", nb1, nb2);
+
+    printf(", ");
+
+    nb = nb1 + nb2;
+    while (nb <= nb3){
+        printf("%d, ", nb);
+        nb1 = nb2;
+        nb2 = nb;
+        nb = nb1 + nb2;
+    }
+
+    
+    return 0;
+}
